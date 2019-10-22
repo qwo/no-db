624278f7cc48a24bc846661623e4e61364cc1217
Stanley Zheng <Stanley@zheng.nyc>
2019-10-21 21:43:49 -0400

    adding lib

--- lib.py
+++ lib.py
@@ -1,20 +1,54 @@
-import sqlite3
-from sqlite3 import Error
 
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
 
+class Fast(object):
 
-if __name__ == "__main__":
-    create_connection(r"db/pythonsqlite.db")
+    def __init__(self, object):
+
+        if type(object) == dict:
+            print('dict')
+        elif type(object) == list:
+            print('list')
+        elif type(object) == set:
+            print('set')
+        elif type(object) == str:
+            print('str')
+        else:
+            print(type(object))
+
+        self.instance = object
+
+Fast([]) 
+Fast('foo')
+Fast({})
+
+# List                list                          list
+# Set                 set                           set
+# Dict                dict                          hash
+# String              string                        string      Mutable - string methods that normally create a new string object in Python will mutate the string stored in Redis
+# ImmutableString     string                        string      Immutable - behaves like a regular Python string
+# Int                 int                           int
+# Float               float                         float
+#     pass
+
+"""
+
+List                list                          list
+Set                 set                           set
+Dict                dict                          hash
+String              string                        string      Mutable - string methods that normally create a new string object in Python will mutate the string stored in Redis
+ImmutableString     string                        string      Immutable - behaves like a regular Python string
+Int                 int                           int
+Float               float                         float
+Queue               Queue.Queue                   list
+LifoQueue           Queue.LifoQueue               list
+SetQueue            N/A                           list + set  Extension of ``Queue`` with unique members
+LifoSetQueue        N/A                           list + set  Extension of ``LifoQueue`` with unique members
+BoundedSemaphore    threading.BoundedSemaphore    list        Extension of ``Queue`` leveraging Redis' blocking list pop operations with timeouts, while using Queue's ``maxsize`` arg to provide BoundedSemaphore's ``value`` arg
+Semaphore           threading.Semaphore           list        Extension of ``BoundedSemaphore`` without a queue size
+Lock                threading.Lock                list        Extension of ``BoundedSemaphore`` with a queue size of 1
+RLock               threading.RLock               list        Extension of ``Lock`` allowing multiple ``acquire`` calls
+DefaultDict         collections.DefaultDict       hash
+MultiSet            collections.Counter           hash
+"""
 
